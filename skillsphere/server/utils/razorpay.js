70f1387e36a751063663d5891900ee4f26ed1f91
Bhawana0218 <bhawanabishtbhanu1205@gmail.com>
2026-04-06 08:59:42 +0530

    Adding razorpay.js file

---
+++ skillsphere/server/utils/razorpay.js
@@ -0,0 +1,11 @@
+import Razorpay from "razorpay";
+
+export const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret:
+    process.env.RAZORPAY_SECRET_KEY ||
+    process.env.RAZORPAY_KEY_SECRET ||
+    process.env.RAZORPAY_SECRET,
+  
+});
+
